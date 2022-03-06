69d9bd15cdc3e7f1b3c87590220a6fa883f02e7f
mariusz <mariusz.dabal@paypo.pl>
2022-03-06 10:20:03 +0100

    Add encore

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
